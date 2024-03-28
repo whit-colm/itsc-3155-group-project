9eea264976e7a0a27ba4ad56bbaa7da930f9ea92
Mina-Yuakeem <157329712+Mina-Yuakeem@users.noreply.github.com>
2024-03-27 21:23:56 -0400

    Add files via upload
    Adding Info Page
    
    Signed-off-by: Mina-Yuakeem <157329712+Mina-Yuakeem@users.noreply.github.com>

---
+++ AskHole-App/src/components/Info.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+import './Info.css';
+
+function Info() {
+  return (
+    <div className='Info-container'>
+        <video src='/video/CollegeSockFootage.mp4' autoPlay loop muted />
+      <h1>Info</h1>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+      <p>------------------------------------</p>
+
+    
+    </div>
+  );
+}
+
+export default Info;
