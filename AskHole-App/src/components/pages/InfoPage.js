6634d90ce1707d33d638fed6659aeb2c71a1976c
Mina-Yuakeem <157329712+Mina-Yuakeem@users.noreply.github.com>
2024-03-27 21:24:26 -0400

    Add files via upload
    Adding Info Page 
    
    Signed-off-by: Mina-Yuakeem <157329712+Mina-Yuakeem@users.noreply.github.com>

---
+++ AskHole-App/src/components/pages/InfoPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Info from '../Info';
+import Footer from '../Footer';
+
+
+
+function InfoPage() {
+  return (
+    <>
+      <Info />
+      <Footer />
+    </>
+  );
+}
+
+export default Info;
