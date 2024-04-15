65e30328ec9cc20b4b33d234579343491d1336b0
[GageGaspar] <[ggaspary@uncc.edu]>
2024-04-15 16:39:18 -0400

    view for GET profile api

--- server/askserver/askserver/views.py
+++ server/askserver/askserver/views.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 from .models import Report
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from .models import User, Thread, Tag
+from django.core.exceptions import ObjectDoesNotExist
+import base64
 
 def get_reports(request):
    
@@ -29,3 +33,24 @@ def get_reports(request):
         'comment': report.comment,
     } for report in page_obj]
     return JsonResponse(reports_data, safe=False)  
+
+#############################################################
+
+@login_required
+def get_user_profile(request):
+    try:
+        user = request.user
+        user_data = {
+            "uid": user.uid,
+            "displayname": base64.b64encode(user.displayname.encode()).decode(),
+            "pronouns": base64.b64encode(user.pronouns.encode()).decode() if user.pronouns else None,
+            "permissions": user.permissions,
+            "threads": [str(thread.id) for thread in Thread.objects.filter(author=user)],
+            "tags": [tag.name for tag in Tag.objects.filter(subscribed_users=user)]
+        }
+        return JsonResponse(user_data, status=200)
+    except ObjectDoesNotExist:
+        return JsonResponse({"message": "User not found"}, status=404)
+
+def unauthorized_response():
+    return JsonResponse({"message": "No token associated with request"}, status=401)
