c2d1d4258f66fe3669571e558071ad8edd4fafa7
[GageGaspar] <[ggaspary@uncc.edu]>
2024-04-15 22:04:59 -0400

    custom user model added to settings

--- server/askserver/askserver/settings.py
+++ server/askserver/askserver/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import askserver
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +50,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+AUTH_USER_MODEL = 'askserver.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
