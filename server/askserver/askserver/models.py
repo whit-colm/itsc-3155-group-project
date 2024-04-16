5c92340715be13ed10c73dfae8b792aeece0f802
[GageGaspar] <[ggaspary@uncc.edu]>
2024-04-15 22:04:35 -0400

    second iteration of models.py
    this iteration was focused more on permissions/access. a custom user model was used here just for testing purposes but to my understanding we are not using this for the final product

--- server/askserver/askserver/models.py
+++ server/askserver/askserver/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+from django.conf import settings
 import uuid
 import os
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.hashers import make_password
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askserver.settings')
 
+class UserManager(BaseUserManager):
+    def create_user(self, uid, password=None, **extra_fields):
+        if not uid:
+            raise ValueError('The given UID must be set')
+        permissions = extra_fields.pop('permissions', 0)
+        user = self.model(uid=uid, permissions=permissions, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class User(models.Model):
-    uid = models.CharField(max_length=255, unique=True)
+class User(AbstractBaseUser, PermissionsMixin):
+    uid = models.CharField(max_length=255, unique=True, primary_key=True)
     displayname = models.CharField(max_length=255, blank=True, null=True)
     pronouns = models.CharField(max_length=50, blank=True, null=True)
-    permissions = models.IntegerField(default=0)
+    permissions = models.IntegerField(default=0)  
+    objects = UserManager()
+    password = models.CharField(max_length=128, default=make_password(None))
+    USERNAME_FIELD = 'uid'
+    REQUIRED_FIELDS = []
 
 
 class Tag(models.Model):
@@ -16,9 +32,10 @@ class Tag(models.Model):
 
 
 class Message(models.Model):
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     thread = models.ForeignKey('Thread', related_name='messages', on_delete=models.CASCADE, null=True, blank=True)
-    author = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
     date = models.DateTimeField()
     votes = models.IntegerField(null=True, blank=True, default=0)
     body = models.TextField()
@@ -28,7 +45,7 @@ class Message(models.Model):
 class Thread(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, related_name='threads', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='threads', on_delete=models.CASCADE)
     date = models.DateTimeField()
     communityaward = models.ForeignKey(Message, related_name='community_award_threads', null=True, blank=True, on_delete=models.SET_NULL)
     authoraward = models.ForeignKey(Message, related_name='author_award_threads', null=True, blank=True, on_delete=models.SET_NULL)
@@ -39,7 +56,8 @@ class Thread(models.Model):
 class Report(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField()
     reason = models.TextField()
     comment = models.TextField(null=True, blank=True)
+
